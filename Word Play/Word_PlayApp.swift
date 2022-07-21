cf1e1da79645af3ab4ece7c18a96b1b5e90a2bc8
Ali Feras <ferasali876@gmail.com>
2022-07-21 10:40:31 -0500

    Initial Commit

---
+++ Word Play/Word_PlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_PlayApp.swift
+//  Word Play
+//
+//  Created by Ali on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Word_PlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
